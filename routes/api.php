c8fad6528f82076765c9ce4e79a1678e2a7c9dc7
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-08 08:27:35 +0300

    Added User Apis routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
 
 /*
@@ -19,4 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/get_users/{id?}', [UserController::class, 'getAllUsers']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/add_user', [UserController::class, 'addUser']);
+Route::post('/update_user', [UserController::class, 'updateUser']);
 

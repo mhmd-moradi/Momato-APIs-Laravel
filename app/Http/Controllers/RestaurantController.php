25e565f61a4221913de66d8125bb7560f2dd29bf
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-08 08:44:23 +0300

    Added get restaurants Api

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Restaurant;
+
 class RestaurantController extends Controller
 {
-    //
+    public function getAllRestaurants($id = null){
+        if($id != null)
+            $restaurants = Restaurant::find($id);
+        else
+            $restaurants = Restaurant::all();
+        
+        return response()->json([
+            "status" => "Success",
+            "restaurants" => $restaurants
+        ], 200);
+    }
 }

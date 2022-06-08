65f86816399d90767b37589193101dfd18b69a9f
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-08 03:03:22 +0300

    Created Users table schema

---
+++ database/migrations/2022_06_08_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('username');
+            $table->longtext('password');
+            $table->string('email');
+            $table->integer('role');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('age');
+            $table->string('gender');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Users');
+    }
+}

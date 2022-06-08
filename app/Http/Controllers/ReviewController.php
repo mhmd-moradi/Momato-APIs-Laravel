be73b11ac43894deadeac6ed65b154efb25b9ae3
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-08 09:39:10 +0300

    Added get reviews Api

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,7 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Review;
+
+use App\Models\User;
+
 class ReviewController extends Controller
 {
-    //
+    public function getAllReviews($id = null){
+        if($id != null)
+            $reviews = User::join('Reviews', 'Users.id', '=', 'Reviews.user_id')
+            ->where('Reviews.restaurant_id', '=', $id)
+            ->get(['Users.*', 'Reviews.*']);
+        else
+            $reviews = User::join('Reviews', 'Users.id', '=', 'Reviews.user_id')
+            ->get(['Users.*', 'Reviews.*']);
+        
+        return response()->json([
+            "status" => "Success",
+            "reviews" => $reviews
+        ], 200);
+    }
 }

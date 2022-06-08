13ea577ca23b4b98cba9bf55e9dc4d44e506c4f6
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-08 03:39:50 +0300

    Created Restaurants table schema

---
+++ database/migrations/2022_06_08_000631_create_restaurants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Restaurants', function (Blueprint $table) {
+            $table->id('restaurant_id');
+            $table->integer('type');
+            $table->integer('created_by');
+            $table->string('restaurant_name');
+            $table->string('location');
+            $table->longtext('image');
+            $table->string('opening_time');
+            $table->string('closing_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Restaurants');
+    }
+}
